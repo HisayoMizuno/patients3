22b5317bc4ad36f8b3d1357fc133ff156e03b393
水野 久代 <hisayo@mizuno-no-macbook-pro.local>
2018-08-13 20:13:37 +0900

    Initial Commit

---
+++ patients/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  patients
+//
+//  Created by 水野 久代 on 2018/08/13.
+//  Copyright © 2018年 水野 久代. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
